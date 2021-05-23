bd230dea26aa80217e0c8bba9ebf1a4e7fa55d9a
Fenicu <Fenicu@fenicu.men>
2021-05-23 14:01:34 +0300

    add examples

---
+++ examples/console.py
@@ -0,0 +1,48 @@
+import asyncio
+import datetime as dt
+import os.path
+
+from aiotracemoeapi import TraceMoe, types
+
+api = TraceMoe()
+# or api = TraceMoe(token="ABC")
+
+
+async def search_anime(path, url):
+    anime_response = await api.search(path, is_url=url)
+    best_result = anime_response.result[0]
+    if isinstance(best_result.anilist, types.AniList):
+        print(f"Anime: https://myanimelist.net/anime/{best_result.anilist.id_mal}")
+
+        if len(best_result.anilist.title) > 0:
+            print("Title:")
+            for k, v in best_result.anilist.title.items():
+                if v is None:
+                    continue
+                print(f"{k}: {v}")
+
+        if len(best_result.anilist.synonyms) > 0:
+            print("Synonyms:")
+            for syn in best_result.anilist.synonyms:
+                print(syn)
+
+        if best_result.anilist.is_adult:
+            print("Hentai 🔞!")
+
+    if best_result.episode:
+        print(f"Episode: {best_result.episode}")
+
+    if best_result.afrom:
+        print(f"Starting time of the matching scene: {str(dt.timedelta(seconds=int(best_result.afrom)))}")
+
+    print("Similarity: {:.1%}".format(best_result.similarity))
+
+
+if __name__ == "__main__":
+    path = os.path.join(os.path.abspath("."), "pics", "bleach.jpg")
+    url = False
+    if not os.path.isfile(path):
+        path = "https://upload.wikimedia.org/wikipedia/en/7/7e/Hollowbankai.jpg"
+        url = True
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(search_anime(path, url))
---
+++ examples/telegrambot.py
@@ -0,0 +1,85 @@
+import datetime as dt
+import io
+
+from aiogram import Bot, Dispatcher, executor, md, types
+from aiotracemoeapi import TraceMoe
+from aiotracemoeapi.types import AniList
+
+API_TOKEN = 'BOT TOKEN HERE'
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+trace_bot = TraceMoe()
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.reply("You can Send / Forward anime screenshots to me.")
+
+
+@dp.message_handler(
+    chat_type=[types.ChatType.PRIVATE],
+    content_types=[types.ContentType.PHOTO],
+    run_task=True
+)
+async def search_anime_private(message: types.Message):
+    msg = await message.answer("Search...")
+    pic = io.BytesIO()
+
+    try:
+        pic = await message.photo[-1].download(pic)
+        anime_response = await trace_bot.search(pic)
+    except Exception:
+        await msg.edit_text(r"Ooops ¯\_(ツ)_/¯")
+        return
+
+    best_result = anime_response.result[0]
+    out = str()
+
+    if isinstance(best_result.anilist, AniList):
+        link = f"https://myanimelist.net/anime/{best_result.anilist.id_mal}"
+
+        if len(best_result.anilist.title) > 0:
+            out += f"{md.hlink('Title:', link)}\n"
+            for k, v in best_result.anilist.title.items():
+                if v is None:
+                    continue
+                out += f"  {k}: {v}\n"
+
+        if len(best_result.anilist.synonyms) > 0:
+            out += "Synonyms:\n"
+            for syn in best_result.anilist.synonyms:
+                out += f"  {syn}\n"
+
+    if best_result.anilist.is_adult:
+        out += "Hentai🔞\n"
+
+    if best_result.episode:
+        out += f"Episode: {md.hbold(str(best_result.episode))}\n"
+
+    if best_result.afrom:
+        out += f"Starting time of the matching scene: {md.hbold(str(dt.timedelta(seconds=int(best_result.afrom))))}\n"
+
+    out += "Similarity: <b>{:.1%}</b>\n".format(best_result.similarity)
+
+    await msg.edit_text(out)
+    await message.chat.do(types.ChatActions.UPLOAD_VIDEO)
+    await message.reply_video(best_result.video)
+
+
+@dp.message_handler(
+    commands="wait",
+    chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP],
+    is_reply=True,
+    run_task=True
+)
+async def search_anime_in_group(message: types.Message, reply: types.Message):
+    allow_list = [types.ContentType.PHOTO]
+    if reply.content_type not in allow_list:
+        await message.answer("This is not a screenshot")
+        return
+    await search_anime_private(reply)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
